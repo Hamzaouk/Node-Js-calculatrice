dc7da3e6a78b9281536d1e1e10b87441adafe6f5
OUKHATOU HAMZA <hamzaoukhatou@gmail.com>
2024-12-12 11:39:04 +0100

    readline interface

---
+++ index.js
@@ -0,0 +1,9 @@
+//importer readline module
+const readline = require("readline");
+
+//readline interface
+const read = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
